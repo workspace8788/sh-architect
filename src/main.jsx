9e9de78a282e622ffa5d7ae6c1c2413d7787cf01
AdeelAhmad2295 <adeelahmad8538@gmail.com>
2026-06-29 14:07:00 +0530

    Add GitHub Pages deployment workflow

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles.css'
 
+const baseName = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
+const redirectPath = new URLSearchParams(window.location.search).get('redirect')
+
+if (redirectPath && baseName) {
+  window.history.replaceState(null, '', `${baseName}${redirectPath}`)
+}
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
