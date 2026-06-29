8328d8f7a6ce460abc3b6b75b41d16c846151e0c
AdeelAhmad2295 <adeelahmad8538@gmail.com>
2026-06-29 15:55:55 +0530

    fixx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles.css'
 
-const baseName = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
-const redirectPath = new URLSearchParams(window.location.search).get('redirect')
-
-if (redirectPath && baseName) {
-  window.history.replaceState(null, '', `${baseName}${redirectPath}`)
-}
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={baseName}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
